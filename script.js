fea17fa00716f4718c6c9492149d9ef37e8a7801
uchqun97 <uchqun1997shoh@gmail.com>
2024-06-17 14:30:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+//const age=+prompt("how old are you","13");
+//if(age>=18){
+  // alert("siz o'smir yoshdasiz");
+
+//}
+//else{
+//    alert("qumingni oyna");
+//}
+//const rang=prompt('rangni krit')
+//if ({rang==='qizil'){
+  //  alert("to'xta")
+
+//}
+//else if(rang=='yashil'){
+  //  alert("olg'a")
+   // else{
+     //   alert('notogri qiymat kiriltigan)
+    //}
+//}
+//(age>=18)?console(log("you are adult")):console.log("you are not adult");
+
+const color='red';
+switch (color){
+    case 'red':
+        console.log("stop");
+        break;
+        case "yellow":
+            console.log("ready");
+            break;
+            case "green":
+                console.log("Go")
+                break;
+                default:
+                    console.log("go home nigge");
+
+}
